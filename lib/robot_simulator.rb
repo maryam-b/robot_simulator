6e621af3c3f9a04d5184f936fced11459367b003
maryam-b <m.bahrami.zj@gmail.com>
2017-08-27 17:17:26 +0200

    add basic structure

--- lib/robot_simulator.rb
+++ lib/robot_simulator.rb
@@ -1,4 +1,8 @@
 require "robot_simulator/version"
+require "robot_simulator/table"
+require "robot_simulator/robot"
+require "robot_simulator/utils/direction"
+
 
 module RobotSimulator
   # Your code goes here...
